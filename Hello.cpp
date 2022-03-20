3fee50e1a5dd7761e1afa69140524a376e7a6898
anonymousdaisy5 <zmeet250@gmail.com>
2022-03-20 15:22:15 +0530

    a

--- Hello.cpp
+++ Hello.cpp
@@ -5,15 +5,15 @@
 // }
 
 // sum of natural number 
-#include <iostream>
-using namespace std;
-int main(){
-    int n , sum;
-    cout<<"enter n :" <<endl;
-    cin>> n;
-    sum= n*(n+1)/2;
-    cout<<"sum is "<<sum;
-}
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n , sum;
+//     cout<<"enter n :" <<endl;
+//     cin>> n;
+//     sum= n*(n+1)/2;
+//     cout<<"sum is "<<sum;
+// }
 
 // Roots of Quadratic eqation
 
